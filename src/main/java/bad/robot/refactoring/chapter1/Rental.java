e1404351c43ba21ddfddbc6b3387b0ffcd184038
Anthony Scott <scotta42@ferris.edu>
2020-05-07 00:53:16 -0400

    Move method used to move the getChargeFor() to the Rental class

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -16,9 +16,29 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
-
+    
     public int getDaysRented() {
         return daysRented;
     }
+    
+    double getChargeFor() {
+        double amount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (rental.getDaysRented() > 2)
+                    amount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    amount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+  	}
 
 }
