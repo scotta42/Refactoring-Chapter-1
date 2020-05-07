e00b413626452f9aa9a1fe11be16e22ac3cc62d0
Anthony Scott <scotta42@ferris.edu>
2020-05-07 00:58:49 -0400

    rental.* and Movie.* where both removed to complete move and rename.

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -23,19 +23,19 @@ public class Rental {
     
     double getChargeFor() {
         double amount = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
+        switch (getMovie().getPriceCode()) {
+            case REGULAR:
                 amount += 2;
-                if (rental.getDaysRented() > 2)
-                    amount += (rental.getDaysRented() - 2) * 1.5;
+                if (getDaysRented() > 2)
+                    amount += (getDaysRented() - 2) * 1.5;
                 break;
-            case Movie.NEW_RELEASE:
-                amount += rental.getDaysRented() * 3;
+            case NEW_RELEASE:
+                amount += getDaysRented() * 3;
                 break;
-            case Movie.CHILDREN:
+            case CHILDREN:
                 amount += 1.5;
-                if (rental.getDaysRented() > 3)
-                    amount += (rental.getDaysRented() - 3) * 1.5;
+                if (getDaysRented() > 3)
+                    amount += (getDaysRented() - 3) * 1.5;
                 break;
         }
         return amount;
